b042a36b9d0660964c39945daba8c39eea2a3c31
Sun Lau <vibe@laxy.travel>
2025-06-26 18:02:45 +0800

    Laxy Guide Init

---
+++ src/api/guide-application-config/controllers/guide-application-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * guide-application-config controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::guide-application-config.guide-application-config');
---
+++ src/api/poi-guide/controllers/poi-guide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * poi-guide controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::poi-guide.poi-guide');
