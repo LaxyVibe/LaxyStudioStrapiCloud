f6c8868b946ef1dfcf17fb0ab769d0a52ba62e36
Sun Lau <sun.lau@tecpal.com>
2025-09-01 05:12:18 +0800

    test botContext

---
+++ src/api/bot-context/routes/bot-context.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bot-context router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::bot-context.bot-context');
